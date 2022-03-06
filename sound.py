803ecea2ddbeae433489d7fd0cfdd5b96110c216
Jerome Swannack <jeromeswannack@gmail.com>
2022-03-06 16:13:31 +1300

    added lower frequencies of audible range

--- sound.py
+++ sound.py
@@ -1,14 +1,15 @@
+import math
 from typing import Callable
 import pyaudio
 import numpy as np
 
 # PyAudio INIT:
-CHUNK = 1024 * 2  # Samples: 1024,  512, 256, 128
+CHUNK = 1024 * 4  # Samples: 1024,  512, 256, 128
 RATE = 44100  # Equivalent to Human Hearing at 40 kHz
 FREQ = np.arange((CHUNK / 2) + 1) / (float(CHUNK) / RATE)
 TIME_STEP = CHUNK / RATE
 
-BREAK_POINTS = np.logspace(2, 5, 12 * 4 + 1)
+BREAK_POINTS = np.logspace(math.log10(20), 5, 12 * 3 + 1)
 BREAK_POINTS = BREAK_POINTS[: sum(BREAK_POINTS <= np.max(FREQ)) + 1]
 
 WAVE_MAX = 2**16
@@ -45,11 +46,15 @@ def calculate_psd(data: np.array, win: np.array = None):
     # convert to dB scale
     psd = 20 * np.log10(psd)
 
-    return [
-        np.max(psd[(FREQ >= f_0) & (FREQ < f_1)])
-        for f_0, f_1 in zip(np.concatenate([[0], BREAK_POINTS[:-1]]), BREAK_POINTS)
-    ]
-
+    output = []
+    value = 1.0
+    for f_0, f_1 in zip(np.concatenate([[0], BREAK_POINTS[:-1]]), BREAK_POINTS):
+        if sum((FREQ >= f_0) & (FREQ < f_1)) > 0:
+            value = np.max(psd[(FREQ >= f_0) & (FREQ < f_1)])
+            
+        output.append(value)
+    return output
+        
 
 def calculate_max_amplitude(data: np.array):
     return np.mean(abs(data)) / WAVE_MAX
